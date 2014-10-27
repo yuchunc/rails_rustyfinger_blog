ecff5fc9495beff57f6e64a8e27554f795a545ec
Mickey Chen <poverwhelming@gmail.com>
2014-10-27 23:01:00 +0800

    added some configurations

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module RustyfingerBlog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.template_engine :slim
+      g.view_specs      false
+      g.helper_specs    false
+    end
   end
 end
