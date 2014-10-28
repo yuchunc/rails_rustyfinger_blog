e37f5a0c884e6e2fdb02a1b4b169239fa334353d
Mickey Chen <poverwhelming@gmail.com>
2014-10-28 22:19:32 +0800

    added factory for post model

---
+++ spec/factories/post.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :post do
+    title {Faker::Lorem.sentence}
+    excerpt {Faker::Lorem.paragraph 2}
+    content {Faker::Lorem.paragraphs 5}
+  end
+end
+
+# in console]
+# FactoryGirl.build :post
+# FactoryGirl.create :post
+# use build_list or create_list for multiple instance
